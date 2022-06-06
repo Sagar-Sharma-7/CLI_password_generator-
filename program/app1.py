e15a5083638f1db937db21c2895176717335cbf8
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2022-06-06 19:11:33 +0530

    diff char combinations

--- program/app1.py
+++ program/app1.py
@@ -1,3 +1,5 @@
+import itertools as it
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -14,8 +16,24 @@ class bcolors:
 print(bcolors.OKBLUE + "Starting Password Generator...")
 print("")
 
+characters = {
+    "uppercase": ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'],
+    "lowercase": ['abcdefghijklmnopqyrstuvwxyz'],
+    "numbers": ['0123456789'],
+    "symbols": ['!@#$&*?_-'],
+}
+
 def gen_pass(n):
-    print(n)
+    print(bcolors.OKGREEN + "Select Character type number from the list: ")
+    arr = ["Numbers", "Uppercases", "Lowercases", "Symbols"]
+    l1 = list(it.combinations(arr, 1))
+    l2 = list(it.combinations(arr, 2))
+    l3 = list(it.combinations(arr, 3))
+    l4 = list(it.combinations(arr, 4))
+    l = l1 + l2 + l3 + l4
+    for i in range(len(l)):
+        print("\t",i+1, " --- ", l[i])
+    
 
 try:
     n = int(input("\tLength of Password: "))
