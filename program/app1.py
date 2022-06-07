3d3dc6dcce7b2105ac8a7f441a14a9879497cd6d
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2022-06-07 08:36:59 +0530

    getting all the password specs from user

--- program/app1.py
+++ program/app1.py
@@ -30,9 +30,22 @@ def gen_pass(n):
     l2 = list(it.combinations(arr, 2))
     l3 = list(it.combinations(arr, 3))
     l4 = list(it.combinations(arr, 4))
-    l = l1 + l2 + l3 + l4
+    l = l1 + l2 + l3 + l4  # list of all the possible combinations of characters
     for i in range(len(l)):
         print("\t",i+1, " --- ", l[i])
+    print(bcolors.OKBLUE + "")
+    while True:
+        c_type = int(input("Type any serial number from the above list of combinations: "))
+        if c_type in range(1, 16):
+            break
+        else:
+            print(bcolors.HEADER + "Serial number should be from 1 to 15...")
+            print(bcolors.OKBLUE + "")        
+     
+     ## Generating password with selected combination
+    s_comb = l[c_type - 1] # getting selected combination
+    s_comb_l = [i for i in list(l[c_type - 1])]
+    print(s_comb_l)
     
 
 
